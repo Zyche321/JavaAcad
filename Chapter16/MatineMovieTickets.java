4eb27c2f8094a40f439b93780a4d31c106ae2834
Zyche <106256003+Zyche321@users.noreply.github.com>
2024-03-06 01:17:22 +0800

    Add files via upload

---
+++ Chapter16/MatineMovieTickets.java
@@ -0,0 +1,28 @@
+package JAVA_ACAD.Chapter16;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		final float normAdult = 8, matineeAdult = 5, normChild = 4, childMatinee = 2;
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.print("Enter your age: ");
+		int age = scan.nextInt();
+		System.out.print("Enter current time in Military Time (exe. 4:30PM is 1630): ");
+		int time = scan.nextInt();
+		
+		if (age > 13) {
+			if (time >= 1300 && time <= 1700)
+				System.out.print ("The price of the ticket is $"+ matineeAdult);
+			else
+				System.out.print ("The price of the ticket is $"+ normAdult);
+		}
+		else {
+			if (time >= 1300 && time <= 1700)
+				System.out.print ("The price of the ticket is $"+ childMatinee);
+			else
+				System.out.print ("The price of the ticket is $"+ normChild);
+		}
+	}
+}
