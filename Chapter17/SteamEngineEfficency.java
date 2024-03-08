5865a46029f37d721be54e63e917605f4c29e87e
Zyche <106256003+Zyche321@users.noreply.github.com>
2024-03-08 22:55:10 +0800

    Add files via upload

---
+++ Chapter17/SteamEngineEfficency.java
@@ -0,0 +1,22 @@
+package Chapter17;
+import java.util.Scanner;
+public class SteamEngineEfficency {
+
+	public static void main(String[] args) {
+		
+		double efficiency = 0.0;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter Air Temperature (in Kelvin): ");
+		double Tair = scan.nextFloat();
+		System.out.print("Enter Steam Temperature (in Kelvin): ");
+		double Tsteam = scan.nextFloat();
+		
+		if (Tsteam >= 373)
+			efficiency = 1.0 - Tair / Tsteam;
+		
+		scan.close();
+		
+		System.out.printf("The maximum possible efficiency: %%%f", efficiency);
+	}
+}
