7d12476840dd7437b3ca31bedc8fe80c81f9463c
Zyche <106256003+Zyche321@users.noreply.github.com>
2024-03-31 22:30:44 +0800

    Update LastChanceGas.java

--- Chapter16/LastChanceGas.java
+++ Chapter16/LastChanceGas.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 public class LastChanceGas {
     public static void main(String[] args) {
-
-        Scanner ducay = new Scanner (System.in);
+        Scanner scan = new Scanner (System.in);
 
         System.out.println("Tank Capacity: ");
-        int tankCapacity = ducay.nextInt();
+        int tankCapacity = scan.nextInt();
         System.out.println("Gage reading: ");
-        int gageReading = ducay.nextInt();
+        int gageReading = scan.nextInt();
         System.out.println("Miles per gallon: ");
-        int milesGallon = ducay.nextInt();
+        int milesGallon = scan.nextInt();
 
         int maximumDistance = (int)(gageReading / 100.0 * tankCapacity) * milesGallon;
-
         if (maximumDistance >= 200)
             System.out.println("Safe to proceed");
         else
