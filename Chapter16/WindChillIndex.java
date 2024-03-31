b910ad556fd1e274b18effe6b0ee46beff8325d7
Zyche <106256003+Zyche321@users.noreply.github.com>
2024-03-31 22:42:55 +0800

    Update WindChillIndex.java

--- Chapter16/WindChillIndex.java
+++ Chapter16/WindChillIndex.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.lang.Math;
 public class WindChillIndex {
    public static void main (String[] args){
-      
       Scanner scan = new Scanner (System.in);
       
       System.out.println("Enter Wind Speed:");
