fca0c99a5141851b9550fd58c00697df7e325a84
Zyche <106256003+Zycheee@users.noreply.github.com>
2024-04-03 23:28:08 +0800

    Update GradingSystem.java

--- PROJECT1/GradingSystem.java
+++ PROJECT1/GradingSystem.java
@@ -1,43 +1,63 @@
-package PROJECT1;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 public class GradingSystem {
    public static void main (String[] args) {
       Scanner scan = new Scanner (System.in);
 	  DecimalFormat numform = new DecimalFormat("##.##"); 
-      String remarks, term;
-      byte scanOption;
+      String remarks, term, inputString;
+      byte scanOption = 0;
+      // This loop runs the code then checks the condition
       do {
-    	 System.out.println("Welcome to Grade Calculator");
-         System.out.print("[1] Compute Midterm Grade");
-         System.out.print("\n[2] Compute Finals Grade");
-         System.out.print("\n[3] Quit App");
-         System.out.print("\nPress 1 or 2 or 3: ");
-         scanOption = scan.nextByte();         
-         if ( scanOption == 1 || scanOption == 2 ) {
-             System.out.print("Enter the quiz grade: ");
-             float quizGrade = scan.nextFloat();
-             System.out.print("Enter the grade: ");
-             float labGrade = scan.nextFloat();
-             if (scanOption == 1)
-            	 term = "Midterm";
-             else
-            	 term = "Finals";
-             System.out.print("Enter the "+term+" exam grade: ");
-             float examGrade = scan.nextFloat();
-             if (quizGrade + labGrade + examGrade > 15.0)
-                System.out.print("There was an error inputting your grade. Please try again.\n");
-             else {
-                float calculatedGrades = (quizGrade + labGrade + examGrade) / 3;
-                System.out.print("\nMIDTERM GRADE: "+ numform.format(calculatedGrades));
-                if (calculatedGrades <= 3.0)
-                   remarks = "PASSED";
-                else
-                   remarks = "FAILED";
-                System.out.println("\nREMARKS: "+ remarks+ "\n");
-             }
+         System.out.print("Welcome to Grade Calculator \n[1] Compute Midterm Grade \n[2] Compute Finals Grade \n[3] Quit App \nPress 1 or 2 or 3: ");
+         //This condition checks if the user input a number 
+         if (scan.hasNextByte()) {
+             scanOption = scan.nextByte();    
+            //This condition checks if the user wants to calculate for the Midterm [1] or Finals [2]
+             if ( scanOption == 1 || scanOption == 2 ) {
+         	 System.out.print("Enter your Quiz Grade: ");
+         	 float quizGrade = scan.nextFloat();
+         	 System.out.print("Enter your Labworks Grade: ");
+         	 float labGrade = scan.nextFloat();
+            //This condition checks if the previous condition chooses Midterm or Finals
+         	 if (scanOption == 1)
+         		term = "Midterm";
+         	 else
+         	 	term = "Finals";
+            
+         	 System.out.print("Enter your "+term+" Exam Grade: ");
+         	 float examGrade = scan.nextFloat();
+            // This condition checks if the grades that was asked exceeds more than 5
+         	 if (quizGrade > 5 || labGrade > 5 || examGrade > 5)
+         		 System.out.println("There was an error inputting your grade. Please try again.\n");
+         	//This block of code calculates the grade that was given by the user
+         	 else {
+         		 double calculatedGrades = ((quizGrade * 0.20) + (labGrade * 0.40) + (examGrade * 0.40));
+         		 System.out.print("\nMIDTERM GRADE: "+ numform.format(calculatedGrades));
+                //This condition checks the grade if it passes or failed
+         		 if (calculatedGrades <= 3.0)
+         			 remarks = "PASSED";
+         		 else
+         			 remarks = "FAILED";
+                
+         		 System.out.println("\nREMARKS: "+ remarks+ "\n");
+         		}
+         	}
+            //This condition determines that it can't exceed more than 3 since there were only three choices that was asked by the program
+           else if (scanOption > 3)
+        	   System.out.println("Please choose between to the options that was instructed.\n");
          }
+         //This condition checks if the user puts a string or words in the asked choices
+        else if (scan.hasNext()) {
+        	inputString = scan.nextLine();
+    		System.out.println("There was an error in your input, Please try again.\n");
+        }
+    	else 
+        	System.out.println("There was an error in your input, Please try again.\n");
       }
+      while (scanOption == 1 || scanOption == 2 || scanOption != 3);
+      System.out.print("---SHUTTING DOWN---");
+   }
+}
       while (scanOption < 3);
       System.out.print("---SHUTTING DOWN---");
    }
